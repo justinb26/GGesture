1877ed40668f123a7c29ab2ba3c37338c83ceb4c
justinbu <justinbu@extrahop.com>
2022-07-30 16:02:16 -0700

    Tweaking gestures, refactoring. TODO: Chording, Diagonals

--- GGesture.lua
+++ GGesture.lua
@@ -21,7 +21,7 @@ minimalHorizontalMovement_WINDOWS = 2500; -- Roughly 1/26 of the screen
 minimalVerticalMovement_WINDOWS = 2500;
 
 minimalHorizontalMovement_MACOS = 200; -- 200px = about 1.8" at 110ppi
-minimalVerticalMovement_MACOS = 200;
+minimalVerticalMovement_MACOS = 100;
 
 
 if (os == "windows") then
@@ -59,20 +59,24 @@ cycleStates = {
 -- ==========================================================================================
 
 -- Macos actions
+buttons = {}
+for i=1,9 do buttons[i] = false end
+  
 
 -- Middle mouse button - Expose
-actions["macos"]["3"] = function() PressAndReleaseMouseButton(2) end
-actions["macos"]["3U"] = function() pressTwoKeys("lctrl", "up") end
-actions["macos"]["3D"] = function() pressTwoKeys("lctrl", "down") end
-actions["macos"]["3L"] = function() pressTwoKeys("lctrl", "left") end
-actions["macos"]["3R"] = function() pressTwoKeys("lctrl", "right") end
+--actions["macos"]["3"] = function() PressAndReleaseMouseButton(2) end -- Note: Button 2, not 3
+--actions["macos"]["3U"] = function() end
+--actions["macos"]["3D"] = function() end
+--actions["macos"]["3L"] = function() end
+--actions["macos"]["3R"] = function() end
 
--- Button 4 () - 
-actions["macos"]["4"] = function() PressAndReleaseMouseButton(4) end
-actions["macos"]["4U"] = function() end
-actions["macos"]["4D"] = function() end
-actions["macos"]["4L"] = function() end
-actions["macos"]["4R"] = function() end
+-- Button 4 (Bottom Right) - Magnet 1/2
+--actions["macos"]["4"] = function() PressAndReleaseMouseButton(4) end
+actions["macos"]["4"] = function() pressTwoKeys("lgui", "equal") end
+actions["macos"]["4U"] = function() pressThreeKeys("lctrl", "lalt", "up") end
+actions["macos"]["4D"] = function() pressThreeKeys("lctrl", "lalt", "down") end
+actions["macos"]["4L"] = function() pressThreeKeys("lctrl", "lalt", "left") end
+actions["macos"]["4R"] = function() pressThreeKeys("lctrl", "lalt", "right")end
 
 -- Button 5 (Bottom middle) - Magnet 1/3
 actions["macos"]["5"] = function() PressAndReleaseMouseButton(5) end
@@ -81,19 +85,20 @@ actions["macos"]["5D"] = function() pressThreeKeys("lctrl", "lalt", "down") end
 actions["macos"]["5L"] = function() pressThreeKeys("lctrl", "lalt", "d") end
 actions["macos"]["5R"] = function() pressThreeKeys("lctrl", "lalt", "g") end
 
--- Button 6 () - 
-actions["macos"]["6"] = function() PressAndReleaseMouseButton(6) end
+-- Button 6 (Bottom Left) - Magnet 1/6 (left)
+actions["macos"]["6"] = function() pressThreeKeys("lctrl", "lalt", "2") end
 actions["macos"]["6U"] = function() pressThreeKeys("lctrl", "lalt", "2") end
-actions["macos"]["6D"] = function() pressThreeKeys("lctrl", "lalt", "5¡") end
+actions["macos"]["6D"] = function() pressThreeKeys("lctrl", "lalt", "2") end
 actions["macos"]["6L"] = function() pressThreeKeys("lctrl", "lalt", "1") end
-actions["macos"]["6R"] = function() pressThreeKeys("lctrl", "lalt", "6") end
+actions["macos"]["6R"] = function() pressThreeKeys("lctrl", "lalt", "3") end
 
--- Button 7 () - 
-actions["macos"]["7"] = function() PressAndReleaseMouseButton(7) end
-actions["macos"]["7U"] = function() end
-actions["macos"]["7D"] = function() end
-actions["macos"]["7L"] = function() end
-actions["macos"]["7R"] = function() end
+-- Button 7 (Top Right) - Expose
+--actions["macos"]["7"] = function() PressAndReleaseMouseButton(9) end
+actions["macos"]["7"] = function() pressTwoKeys("lgui", "minus") end
+actions["macos"]["7U"] = function() pressTwoKeys("lctrl", "up") end
+actions["macos"]["7D"] = function() pressTwoKeys("lctrl", "down") end
+actions["macos"]["7L"] = function() pressTwoKeys("lctrl", "left") end
+actions["macos"]["7R"] = function() pressTwoKeys("lctrl", "right") end
 
 -- Button 8 (Top middle) - Magnet 1/2
 actions["macos"]["8"] = function() PressAndReleaseMouseButton(8) end
@@ -102,13 +107,12 @@ actions["macos"]["8D"] = function() pressThreeKeys("lctrl", "lalt", "down") end
 actions["macos"]["8L"] = function() pressThreeKeys("lctrl", "lalt", "left") end
 actions["macos"]["8R"] = function() pressThreeKeys("lctrl", "lalt", "right") end
 
--- Button 9 () - 
-actions["macos"]["9"] = function() PressAndReleaseMouseButton(9) end
-actions["macos"]["9U"] = function() end
-actions["macos"]["9D"] = function() end
-actions["macos"]["9L"] = function() end
-actions["macos"]["9R"] = function() end
-
+-- Button 9 (Top left) - Magnet 1/6 (Right)
+actions["macos"]["9"] = function() pressThreeKeys("lctrl", "lalt", "5") end
+actions["macos"]["9U"] = function() pressThreeKeys("lctrl", "lalt", "5") end
+actions["macos"]["9D"] = function() pressThreeKeys("lctrl", "lalt", "5") end
+actions["macos"]["9L"] = function() pressThreeKeys("lctrl", "lalt", "4") end
+actions["macos"]["9R"] = function() pressThreeKeys("lctrl", "lalt", "6") end
 
 -- ==========================================================================================
 
@@ -133,6 +137,7 @@ function OnEvent(event, arg, family)
     buttonNumber = arg
     
     if event == "MOUSE_BUTTON_PRESSED" then
+              buttons[buttonNumber] = true
 		if debuggingEnabled then OutputLogMessage("\nEvent: " .. event .. " for button: " .. buttonNumber .. "\n") end
 		
 		-- Get starting mouse position
@@ -147,6 +152,7 @@ function OnEvent(event, arg, family)
     -- =============================
 
 	if event == "MOUSE_BUTTON_RELEASED" then
+	        buttons[buttonNumber] = false
 		if debuggingEnabled then OutputLogMessage("\nEvent: " .. event .. " for button: " .. buttonNumber .. "\n") end
 		
 		-- Get ending mouse Position
@@ -178,6 +184,13 @@ function OnEvent(event, arg, family)
         
         direction = nil
 	end
+	
+	if debuggingEnabled then
+	  OutputLogMessage("Button states: ")
+	  for i=1,9 do OutputLogMessage(tostring(buttons[i] and 1 or 0)) end
+	  OutputLogMessage("\n")
+	end
+	
 end
 
 -- ==========================================================================================
@@ -190,7 +203,7 @@ function pressTwoKeys(firstKey, secondKey)
 	Sleep(delay)
 	ReleaseKey(secondKey)
 	Sleep(delay)
-	ReleaseKey(firstKey)
+	ReleaseKey(firstKey)  
 end
 
 -- Helper Functions
